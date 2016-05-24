469ab61e8f680907e6a8cef53a1c90c943cecbd9
Kevin Pheasey <kevin@kpheasey.com>
2016-05-24 17:08:53 -0400

    Set SavingsRequiredCalculator spec expected values.

--- spec/services/savings_required_calculator_spec.rb
+++ spec/services/savings_required_calculator_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe SavingsRequiredCalculator do
+RSpec.describe SavingsRequiredCalculator do
 
   let!(:src) do
     SavingsRequiredCalculator.new(
@@ -24,39 +24,39 @@ Rspec.describe SavingsRequiredCalculator do
   end
 
   describe '#portfolio_after_growth' do
-    it { expect(src.portfolio_after_growth).to eq nil }
+    it { expect(src.portfolio_after_growth).to eq 3840000.0 }
   end
 
   describe '#withdrawal_rate' do
-    it { expect(src.withdrawal_rate).to eq nil }
+    it { expect(src.withdrawal_rate).to eq 0.038 }
   end
 
   describe '#adjusted_target_income' do
-    it { expect(src.adjusted_target_income).to eq nil }
+    it { expect(src.adjusted_target_income).to eq 27440.0 }
   end
 
   describe '#savings_portion_needed' do
-    it { expect(src.savings_portion_needed).to eq nil }
+    it { expect(src.savings_portion_needed).to eq 19940.0 }
   end
 
   describe '#annualized_portfolio_amount_needed' do
-    it { expect(src.annualized_portfolio_amount_needed).to eq nil }
+    it { expect(src.annualized_portfolio_amount_needed).to eq 239280.0 }
   end
 
   describe '#recommended_min_portfolio' do
-    it { expect(src.recommended_min_portfolio).to eq nil }
+    it { expect(src.recommended_min_portfolio.round(2)).to eq 6296842.11 }
   end
 
   describe '#needed_amount_less_savings' do
-    it { expect(src.needed_amount_less_savings).to eq nil }
+    it { expect(src.needed_amount_less_savings.round(2)).to eq 2456842.11 }
   end
 
   describe '#required_annual_savings' do
-    it { expect(src.required_annual_savings).to eq nil }
+    it { expect(src.required_annual_savings.round(2)).to eq 204736.84 }
   end
 
   describe '#shortfall_excess' do
-    it { expect(src.shortfall_excess).to eq nil }
+    it { expect(src.shortfall_excess.round(2)).to eq -3896842.11 }
   end
 
 end
